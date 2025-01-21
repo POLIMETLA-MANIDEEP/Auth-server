927097201cdeac5e905e358aa5d42818742cfda0
MANIDEEP <143174316+POLIMETLA-MANIDEEP@users.noreply.github.com>
2025-01-21 16:07:00 +0530

    Update Student.js

--- Student.js
+++ Student.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const StudentSchema = new mongoose.Schema({
     name: String,
-    email: String,
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
     password: String
 });
 
 const StudentModel = mongoose.model("students", StudentSchema);
 
-// Use CommonJS export
 module.exports = StudentModel;
